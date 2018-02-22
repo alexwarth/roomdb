ba0bbe5bc30dc3a3f889a6da73b76227969673ee
Jonathan Dahan <hi@jonathan.is>
2018-02-22 17:30:12 -0500

    updates

--- tests/basic.js
+++ tests/basic.js
@@ -4,15 +4,17 @@ const room = new RoomDB()
 const client = room.connect()
 
 test('basics', t => {
-  client
-    .assert('#gorog is a barbarian at 50, 50')
+  client.assert('#gorog is a barbarian at 40, 50')
+  client.retract('#gorog is a barbarian at $x, $y')
+  client.assert('#gorog is a barbarian at 99, 11')
+  client.flushChanges()
 
   client
     .select('$name is a $what at $x, $y')
     .do(({name, what, x, y}) => {
       t.is(name.name, 'gorog')
       t.is(what.str, 'barbarian')
-      t.is(x, 50)
-      t.is(y, 50)
+      t.is(x, 99)
+      t.is(y, 11)
     })
 })
